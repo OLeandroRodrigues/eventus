d7ff854e383624f8944c335fee9d23208a55da3e
Leandro Rodrigues <91847576+OLeandroRodrigues@users.noreply.github.com>
2025-12-30 16:49:38 -0300

    feat(eventus-examples): Add minimal runnable main entry point

---
+++ eventus-examples/src/main/java/io/github/oleandrorodrigues/eventus/examples/Main.java
@@ -0,0 +1,7 @@
+package io.github.oleandrorodrigues.eventus.examples;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Eventus examples running");
+    }
+}
